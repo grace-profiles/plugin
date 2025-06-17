85c13441958fd702bfd5be64c37b56dce3312a9a
Michael Yan <rain@rainboyan.com>
2025-06-17 15:31:09 +0800

    Add `@CompileStatic` to Application
    Closes gh-48

--- skeleton/app/init/@grace.codegen.defaultPackage.path@/Application.groovy
+++ skeleton/app/init/@grace.codegen.defaultPackage.path@/Application.groovy
@@ -1,9 +1,11 @@
 package @grace.codegen.defaultPackage@
 
+import groovy.transform.CompileStatic
 import grails.boot.Grails
 import grails.plugins.metadata.PluginSource
 
 @PluginSource
+@CompileStatic
 class Application {
 
     static void main(String[] args) {
